ea3e73f32b2deed19f0d0ffdc23e777559e8694d
Sophia <44215812+SophFru@users.noreply.github.com>
2019-03-03 18:29:29 -0600

    Format change
    deleted tab, added a line break

--- GitTest2.java
+++ GitTest2.java
@@ -3,7 +3,9 @@ public class GitTest2 {
     public static void main(String[] args) {
 
     	System.out.println("HEEEEEEY SANIE!");
-		System.out.println("YOOOOOO SOOOOPHHHH!!");
+	    
+	System.out.println("YOOOOOO SOOOOPHHHH!!");
+	    
     }
 
 }

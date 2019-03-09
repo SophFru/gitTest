170a47383ccec6e8b5f481cb4a5a5753f1ebd878
S3RA <shinejusa7@gmail.com>
2019-03-08 20:29:02 -0600

    learning branchees

--- WinTemplate.py
+++ WinTemplate.py
@@ -5,6 +5,11 @@ win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("Hello")
 
+x = 5
+y = 10
+width = 50
+height = 50
+
 run = True
 while run:
     pygame.time.delay(100)
@@ -12,6 +17,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (255, 0, 0), (x,y, width, height))
     
     pygame.display.update()
 
